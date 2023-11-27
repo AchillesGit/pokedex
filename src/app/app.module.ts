d4cfc63e27fdc21bfd5f62055c520616564dbbd1
AchillesGit <dennisadiki@gmail.com>
2023-11-27 18:49:56 +0100

    Add new  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
+import { PokemonFilterComponent } from './components/pokemon-filter/pokemon-filter.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { PokemonStatsComponent } from './components/pokemon-stats/pokemon-stats.component';
+import { PokemonEvolutionComponent } from './components/pokemon-evolution/pokemon-evolution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonSearchComponent,
+    PokemonFilterComponent,
+    PokemonListComponent,
+    PokemonCardComponent,
+    PokemonDetailComponent,
+    PokemonStatsComponent,
+    PokemonEvolutionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pokemon-evolution/pokemon-evolution.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-evolution',
+  templateUrl: './pokemon-evolution.component.html',
+  styleUrls: ['./pokemon-evolution.component.scss']
+})
+export class PokemonEvolutionComponent {
+
+}
